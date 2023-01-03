5281e53cd3089f70bc357c9aa3df8ad776e13940
Conor McManamon <c.s.mcmanamon@gmail.com>
2023-01-03 12:28:40 -0500

    First function

--- js/exercises.js
+++ js/exercises.js
@@ -5,7 +5,13 @@
   // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in Javascript.
   // ---------------------
 
-
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
